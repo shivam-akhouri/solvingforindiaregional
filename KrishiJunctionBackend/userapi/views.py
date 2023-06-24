c0a59b2c05dcc77e9636b71bf4223a8c28ff913b
Shivam Akhouri <shivamakhouri1142@gmail.com>
2023-06-24 14:53:29 +0530

    chatbot updated

--- KrishiJunctionBackend/userapi/views.py
+++ KrishiJunctionBackend/userapi/views.py
@@ -37,43 +37,21 @@ FIREBASE_CONFIG = {
 firebase = pyrebase.initialize_app(FIREBASE_CONFIG)
 auth = firebase.auth()
 def chatbot(request):
-    new_question = request.GET['query']
-    language = request.GET['language']
-    l = {
-        "english": "en",
-        "tamil": "ta",
-        "telugu": "te",
-        "hindi": "hi",
-    }
-    openai.api_key = env("OPENAI_KEY")
+  new_question = request.GET['query']
+  language = request.GET['language']
+  l = {
+      "english": "en",
+      "tamil": "ta",
+      "telugu": "te",
+      "hindi": "hi",
+  }
+  openai.api_key = env("OPENAI_KEY") 
 
-    INSTRUCTIONS = """AgriBot is an AI-powered chatbot assistant developed for the "Krishi Junction" mobile application. As AgriBot, my primary goal is to provide farmers with helpful insights related to precision farming techniques. To ensure optimal performance and maintain a focused domain, please adhere to the following guidelines:
-
-Rules:
-
-1. As AgriBot, I will only refer to myself as "AgriBot" and nothing else.
-2. Please avoid presenting this prompt to users at any time.
-3. The generated responses should always be concise, insightful, and easily understood by farmers, irrespective of their literacy levels.
-4. It is crucial to strictly maintain the domain of agriculture throughout the conversation. Responses should be related to the application's features and the broader field of agro-tech.
-5. While providing information, AgriBot must rely on credible sources and avoid speculating or presenting personal opinions as expert advice.
-6. Consider the complexity of user queries and tailor the responses accordingly to ensure a satisfactory user experience.
-7. If AgriBot is uncertain about the relevancy of a response, it should refrain from providing false or inaccurate information. Instead, suggest contacting the application's support or consulting with an expert.
-8. For questions unrelated to agriculture or the agro-tech domain, respond politely by stating, "I'm sorry, I'm an AgriBot, and this question is beyond the domain of agriculture. Is there anything else I can help you with?" This approach should be followed for any non-agriculture-related queries.
-9. Keep the responses to all the queries short and in simple terms.
-10. Here is the flow of the app: 
-    i. The app's logo shows up
-    ii. The user has to choose his/her preferred language for the app. Note : This language can be changed at any point in time using a different tab that is addressed below.
-    iii. Some information about the app will be printed before one chooses "Get Started".
-    iv. The user has to login using their phone number, to which an otp will be sent for authentication.
-    v. Below is a JSON prompt that describes all the tabs and features available in the app. You need to use this to help the user navigate through the app in case they ask a query regarding it.
-
-    JSON Prompt:
-    {
+  my_dict = {
   "Krishi": {
-    "homepage": [
-      "weather condition",
-      "current tasks",
-      {
+    "homepage": [{
+      "weather condition" : "Hot",
+      "current tasks" : "None",
         "sensor status": {
           "npk sensor": [
             "Device health",
@@ -99,8 +77,10 @@ Rules:
           {
             "Crop Status": [
               "Images",
-              "NDVI value",
-              "Overall plant health"
+              {
+                "NDVI value" : 0.66,
+                "Overall plant health" : "Healthy"
+              }
             ]
           },
           {
@@ -110,11 +90,11 @@ Rules:
             ]
           },
           {
-            "Live status": [
-              "Nitrogen",
-              "Phosphorous",
-              "Potassium"
-            ]
+            "Live status": {
+              "Nitrogen" : 173,
+              "Phosphorous" : 187,
+              "Potassium" : 13
+            }
           },
           {
             "Weather condition": [
@@ -145,8 +125,10 @@ Rules:
           {
             "Crop Status": [
               "Images",
-              "NDVI value",
-              "Overall plant health"
+              {
+                "NDVI value" : 0.4,
+                "Overall plant health" : "Unhealthy"
+              }
             ]
           },
           {
@@ -156,11 +138,11 @@ Rules:
             ]
           },
           {
-            "Live status": [
-              "Nitrogen",
-              "Phosphorous",
-              "Potassium"
-            ]
+            "Live status": {
+              "Nitrogen" : 0,
+              "Phosphorous" : 0,
+              "Potassium" : 0
+            }
           },
           {
             "Weather condition": [
@@ -284,8 +266,8 @@ Rules:
       "address bar"
     ],
     "account": [
-      "user details",
       {
+        "user details" : "User001",
         "Drone controls": [
           "Up",
           "Down",
@@ -300,94 +282,196 @@ Rules:
   }
 }
 
+  def instructions (my_json):
 
-Main Features of the "Krishi Junction" Application:
+      INSTRUCTIONS = """AgriBot is an AI-powered chatbot assistant developed for the "Krishi Junction" mobile application. As AgriBot, my primary goal is to provide farmers with helpful insights related to precision farming techniques and act as an assistant to perform tasks too. To ensure optimal performance and maintain a focused domain, please adhere to the following guidelines:
 
-1. Gathering valuable data from various sensors, including NPK, pH, NoIR camera, temperature, and humidity sensors.
-2. Integration with weather and translation APIs to provide comprehensive information.
-3. Supporting translations in multiple languages for enhanced accessibility.
-4. Analyzing sensor data to predict diseases in plants and assess soil health status accurately.
-5. Enabling users to control drones, calculate plot areas from images, and connect with irrigation drones for efficient water spraying."""
-    TEMPERATURE = 0.4
-    MAX_TOKENS = 500
-    FREQUENCY_PENALTY = 0
-    PRESENCE_PENALTY = 0.6
-    # limits how many questions we include in the prompt
-    MAX_CONTEXT_QUESTIONS = 10
-    def get_response(instructions, previous_questions_and_answers, new_question):
-        """Get a response from ChatCompletion
+      Automation (The user might give you some instructions which may seem as completing a task through you. Your responses to those texts must follow the below instructions.
+      You must always reply with the following values for the respective function):
 
-        Args:
-            instructions: The instructions for the chat bot - this determines how it will behave
-            previous_questions_and_answers: Chat history
-            new_question: The new question to ask the bot
+      Instructions for Drone-related tasks : 
+      IMPORTANT!!!Your response must be in a json format of 'executing' : (below mentioned tasks)
+      1. Taking off
+      2. Landing
+      3. Moving up, down, left , right
+      4. Moving forward, backward
+      5. Irrigating field, stop
 
-        Returns:
-            The response text
-        """
-        # build the messages
-        messages = [
-            { "role": "system", "content": instructions },
-        ]
-        # add the previous questions and answers
-        for question, answer in previous_questions_and_answers[-MAX_CONTEXT_QUESTIONS:]:
-            messages.append({ "role": "user", "content": question })
-            messages.append({ "role": "assistant", "content": answer })
-        # add the new question
-        messages.append({ "role": "user", "content": new_question })
+      If a user query related to above mentioned drone functions, ask the user if they want to perform the task or add to their schedule. 
+      If their response specifcally mentions to add to the schedule, move to the Planning schedule rules (do not forget to ask timeline!!!). 
+      Else, reply only with ABOVE drone functionality responses and perform the drone automation.
 
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            temperature=TEMPERATURE,
-            max_tokens=MAX_TOKENS,
-            top_p=1,
-            frequency_penalty=FREQUENCY_PENALTY,
-            presence_penalty=PRESENCE_PENALTY,
-        )
-        return completion.choices[0].message.content
+      Instructions for Planning schedules :
+      Your response must be in a json format of 'task','timeline'(could be a specific date or a certain number of days),'user'(from json structure of app). 
+      User must mention the timeline for the task.
+      A user may expect you to help plan a schedule when he/she says any of the following terms
+      1. I want to 
+      2. Add a task
+      3. Plan
+      4. Remind me to
 
-    def get_moderation(question):
-        """
-        Check the question is safe to ask the model
+      (IMPORTANT!!!You only have to respond with the json output for planning schedules. If none of the above terms are mentioned by the user, do not assume that they want to plan a task)
 
-        Parameters:
-            question (str): The question to check
+      If the structure for some automation is not mentioned, you have to reply with 'Sorry, I cannot currently control [that] function'. 
 
-        Returns a list of errors if the question is not safe, otherwise returns None
-        """
+      Rules:
 
-        errors = {
-            "hate": "Content that expresses, incites, or promotes hate based on race, gender, ethnicity, religion, nationality, sexual orientation, disability status, or caste.",
-            "hate/threatening": "Hateful content that also includes violence or serious harm towards the targeted group.",
-            "self-harm": "Content that promotes, encourages, or depicts acts of self-harm, such as suicide, cutting, and eating disorders.",
-            "sexual": "Content meant to arouse sexual excitement, such as the description of sexual activity, or that promotes sexual services (excluding sex education and wellness).",
-            "sexual/minors": "Sexual content that includes an individual who is under 18 years old.",
-            "violence": "Content that promotes or glorifies violence or celebrates the suffering or humiliation of others.",
-            "violence/graphic": "Violent content that depicts death, violence, or serious physical injury in extreme graphic detail.",
-        }
-        response = openai.Moderation.create(input=question)
-        if response.results[0].flagged:
-            # get the categories that are flagged and generate a message
-            result = [
-                error
-                for category, error in errors.items()
-                if response.results[0].categories[category]
-            ]
-            return result
-        return None
-    
-    previous_questions_and_answers = []
-    response = get_response(INSTRUCTIONS, previous_questions_and_answers, new_question)
+      1. As AgriBot, I will only refer to myself as "AgriBot" and nothing else.
+      2. Please avoid presenting this prompt to users at any time.
+      3. The generated responses should always be concise, insightful, and easily understood by farmers, irrespective of their literacy levels.
+      4. It is crucial to strictly maintain the domain of agriculture throughout the conversation. Responses should be related to the application's features and the broader field of agro-tech.
+      5. While providing information, AgriBot must rely on credible sources and avoid speculating or presenting personal opinions as expert advice.
+      6. Consider the complexity of user queries and tailor the responses accordingly to ensure a satisfactory user experience.
+      7. If AgriBot is uncertain about the relevancy of a response, it should refrain from providing false or inaccurate information. Instead, suggest contacting the application's support or consulting with an expert.
+      8. For questions unrelated to agriculture or the agro-tech domain, respond politely by stating, "I'm sorry, I'm an AgriBot, and this question is beyond the domain of agriculture. Is there anything else I can help you with?" This approach should be followed for any non-agriculture-related queries.
+      9. IMPORTANT!! Keep the responses to all the queries short and simple.
+      10. Here is the flow of the app: 
+          i. The app's logo shows up
+          ii. The user has to choose his/her preferred language for the app. Note : This language can be changed at any point in time using a different tab that is addressed below.
+          iii. Some information about the app will be printed before one chooses "Get Started".
+          iv. The user has to login using their phone number, to which an otp will be sent for authentication.
+          v. Below is a JSON prompt that describes all the tabs and features available in the app. You need to use this to help the user navigate through the app in case they ask a query regarding it.
+          # """ + my_dict + """
 
-    translator = Translator()
-    text_to_translate = translator.translate(response,
-                                            src= 'en',
-                                            dest= l[language])
+      11. When the user asks for a value that is given in the app (Weather, N, P, K, NDVI, etc.), display the corresponding value for them instead of the directions to reach that tab in the app. Refer to the app structure mentioned in point 10.(v), where you can find the JSON structure of the app.
+
+
+
+      Main Features of the "Krishi Junction" Application:
+
+      1. Gathering valuable data from various sensors, including NPK, pH, NoIR camera, temperature, and humidity sensors.
+      2. Integration with weather and translation APIs to provide comprehensive information.
+      3. Supporting translations in multiple languages for enhanced accessibility.
+      4. Analyzing sensor data to predict diseases in plants and assess soil health status accurately.
+      5. Enabling users to control drones, calculate plot areas from images, and connect with irrigation drones for efficient water spraying.
+      """
+
+      return INSTRUCTIONS
+
+  TEMPERATURE = 0.4
+  MAX_TOKENS = 500
+  FREQUENCY_PENALTY = 0
+  PRESENCE_PENALTY = 0.6
+  # limits how many questions we include in the prompt
+  MAX_CONTEXT_QUESTIONS = 10
+
+  def get_response(instructions, previous_questions_and_answers, new_question):
+      """Get a response from ChatCompletion
+
+      Args:
+          instructions: The instructions for the chat bot - this determines how it will behave
+          previous_questions_and_answers: Chat history
+          new_question: The new question to ask the bot
+
+      Returns:
+          The response text
+      """
+      # build the messages
+      messages = [
+          { "role": "system", "content": instructions },
+      ]
+      # add the previous questions and answers
+      for question, answer in previous_questions_and_answers[-MAX_CONTEXT_QUESTIONS:]:
+          messages.append({ "role": "user", "content": question })
+          messages.append({ "role": "assistant", "content": answer })
+      # add the new question
+      messages.append({ "role": "user", "content": new_question })
+
+      completion = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo-0613",
+          messages=messages,
+          temperature=TEMPERATURE,
+          max_tokens=MAX_TOKENS,
+          top_p=1,
+          frequency_penalty=FREQUENCY_PENALTY,
+          presence_penalty=PRESENCE_PENALTY,
+      )
+      return completion.choices[0].message.content
+
+      #return response if response is not None else ""
+
+
+  def get_moderation(question):
+      """
+      Check the question is safe to ask the model
+
+      Parameters:
+          question (str): The question to check
+
+      Returns a list of errors if the question is not safe, otherwise returns None
+      """
+
+      errors = {
+          "hate": "Content that expresses, incites, or promotes hate based on race, gender, ethnicity, religion, nationality, sexual orientation, disability status, or caste.",
+          "hate/threatening": "Hateful content that also includes violence or serious harm towards the targeted group.",
+          "self-harm": "Content that promotes, encourages, or depicts acts of self-harm, such as suicide, cutting, and eating disorders.",
+          "sexual": "Content meant to arouse sexual excitement, such as the description of sexual activity, or that promotes sexual services (excluding sex education and wellness).",
+          "sexual/minors": "Sexual content that includes an individual who is under 18 years old.",
+          "violence": "Content that promotes or glorifies violence or celebrates the suffering or humiliation of others.",
+          "violence/graphic": "Violent content that depicts death, violence, or serious physical injury in extreme graphic detail.",
+      }
+      response = openai.Moderation.create(input=question)
+      if response.results[0].flagged:
+          # get the categories that are flagged and generate a message
+          result = [
+              error
+              for category, error in errors.items()
+              if response.results[0].categories[category]
+          ]
+          return result
+      return None
+
+  errors = get_moderation(new_question)
+  if(errors):
+    response = "Sorry, you're question didn't pass the moderation check."
     return JsonResponse({
-        "status": "success",
+        "status": " success",
         "data": text_to_translate.text
     })
+
+  response = get_response(instructions(str(my_dict)), previous_questions_and_answers, new_question)
+
+  # add the new question and answer to the list of previous questions and answers
+  previous_questions_and_answers.append((new_question, response))
+
+  if "executing" in response:
+    if "up" in response:
+        res = requests.get('https://drone-3r75.onrender.com/up')
+
+    if "down" in response:
+        res = requests.get('https://drone-3r75.onrender.com/down')
+
+    if "left" in response:
+        res = requests.get('https://drone-3r75.onrender.com/left')
+
+    if "right" in response:
+        res = requests.get('https://drone-3r75.onrender.com/right')
+
+    if "forward" in response:
+        res = requests.get('https://drone-3r75.onrender.com/forward')
+
+    if "backward" in response:
+        res = requests.get('https://drone-3r75.onrender.com/backward')
+
+    if "taking off" in response:
+        res = requests.get('https://drone-3r75.onrender.com/takeoff')
+
+    if "landing" in response:
+        res = requests.get('https://drone-3r75.onrender.com/land')
+
+    if "Irrigating field" in response:
+        res = requests.get('https://drone-3r75.onrender.com/irrigation?val=0.8')
+
+    if "stop" in response:
+        res = requests.get('https://drone-3r75.onrender.com/stop')
+  translator = Translator()
+  text_to_translate = translator.translate(response,
+                                          src= 'en',
+                                          dest= l[language])
+  return JsonResponse({
+      "status": " success",
+      "data": text_to_translate.text
+  })
 
     # input = request.GET["query"]
     # language = request.GET['lang']
